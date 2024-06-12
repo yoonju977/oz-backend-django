93e768ff55e553e966900a978bea395f6f2d205c
yoonju977 <yoonju977@gmail.com>
2024-06-12 16:46:40 +0900

    django

---
+++ config/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("feeds/", include("feeds.urls")), # 경로 입력
+]
---
+++ feeds/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def show_feed(request):
+    return HttpResponse("show feed")
+def one_feed(request, feed_id, feed_content):
+	return HttpResponse(f"feed id: {feed_id}, feed_content: {feed_content}")
+#f스트링으로 입력받은 값 리턴
+
+def all_feed(request):
+	return HttpResponse("all feed")
